7f7c4278f37440e2258f168e795eda2d263be1dd
Pedro <pedrojuniorifpb@gmail.com>
2022-06-11 13:50:24 -0300

    Limpando codigo comentado

--- js/script.js
+++ js/script.js
@@ -73,35 +73,6 @@ export function inputTextElement(elementId) {
     return elementText;
 }
 
-// function pesquisar() {
-//     let texto = document.querySelector("#entrada-pesquisa").value;
-//     let listaFiltrada = listaTarefas.filter((elementId) => filterFunction(texto, elementId));
-//     retirarElementos(listaTarefas);
-//     adicionarElementos(listaFiltrada);
-// }
-
-// function filterFunction(value, elementID) {
-//     let elementText = inputTextElement(elementID).value;
-//     elementText = elementText.toLowerCase();
-//     if (elementText.indexOf(value.toLowerCase()) != -1) {
-//         return true;
-//     }
-// }
-
-// function retirarElementos(lista) {
-//     for (let cont = 0; cont < lista.length; cont++) {
-//         let element = document.querySelector("#" + listaTarefas[cont]);
-//         element.classList.add("ocultarElemento");
-//     }
-// }
-
-// function adicionarElementos(lista) {
-//     for (let cont = 0; cont < lista.length; cont++) {
-//         let element = document.querySelector("#" + lista[cont]);
-//         element.classList.remove("ocultarElemento");
-//     }
-// }
-
 function adicionarTarefaNaPagina() {
     let main = document.querySelector("#container-tarefas");
     let section = document.createElement("section");
